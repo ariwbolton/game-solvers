2ebf94f9369895e57e366690f7e67a96cc644831
Ari Bolton <ariwbolton@gmail.com>
2024-04-27 23:17:54 -0700

    Fill out query methods

--- WikipediaGameV2/wikipedia-game-v2/WikipediaAPI.py
+++ WikipediaGameV2/wikipedia-game-v2/WikipediaAPI.py
@@ -1,9 +1,41 @@
-
+import requests
 
 WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php"
 
-# https://en.wikipedia.org/w/api.php?action=query&pageids=12345|67890|55555|55|555|5555|1111|11111|1111&prop=links|linkshere&pllimit=max&lhlimit=max&format=json
+# Sample URLs
+
+# Load links + backlinks for a list of pages, by page ID, in JSON
+# https://en.wikipedia.org/w/api.php?action=query&pageids=12345|67890&prop=links|linkshere&pllimit=max&lhlimit=max&format=json
+
+# Load rich links (which include both a pageid and name) for a list of pages
+# https://en.wikipedia.org/w/api.php?action=query&generator=links&pageids=12345&gpllimit=max&prop=info&format=json
 
 class WikipediaAPI:
-    def query(self, *, pageids: list[int], prop: list[str] | str, format: str = 'json') -> dict:
-        pass
+
+    ########
+    # Core #
+    ########
+
+    def query_simple(self, *, pageids: list[int], prop: list[str] | str, format: str = 'json') -> dict:
+        r = requests.get(WIKIPEDIA_API_URL, params={
+            "action": "query",
+            "pageids": "|".join(str(pageid) for pageid in pageids),
+            "prop": "|".join(prop) if isinstance(prop, list) else prop,
+            "format": format,
+            "pllimit": "max",
+            "hllimit": "max"
+        })
+
+        return r.json()
+
+    def query_generator(self, *, pageids: list[int], generator: str, prop: str, format: str = 'json') -> dict:
+        r = requests.get(WIKIPEDIA_API_URL, params={
+            "action": "query",
+            "pageids": "|".join(str(pageid) for pageid in pageids),
+            "generator": generator,
+            "prop": prop,
+            "format": format,
+            "gpllimit": "max",
+        })
+
+        return r.json()
